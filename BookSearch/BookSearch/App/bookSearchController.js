9fe5dfd02e95d39f06aa278369df1ed7fde8fd84
Yiding <yiding.cdm.depaul@hotmail.com>
2015-07-07 16:53:23 -0500

    Able to search and show results as grid list

---
+++ BookSearch/BookSearch/App/bookSearchController.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    function bookSearchController($location, $http) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.searchBook = function() {
+            $http.get(vm.googleApiUrl + vm.searchQuery).then(function(result) {
+                vm.books= result.data.items;
+            });
+        }
+        function activate() {
+            vm.searchQuery = "";
+            vm.googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+        }
+
+        activate();
+    }
+
+    angular
+        .module('app')
+        .controller('BookSearchController', bookSearchController);
+
+    bookSearchController.$inject = ['$location', '$http'];
+})();
