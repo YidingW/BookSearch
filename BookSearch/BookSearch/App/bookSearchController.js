dda88f82bbfdcfa924b31be26438de38d1bbbc4d
Yiding <yiding.cdm.depaul@hotmail.com>
2015-07-10 10:14:29 -0500

    Update

--- BookSearch/BookSearch/App/bookSearchController.js
+++ BookSearch/BookSearch/App/bookSearchController.js
@@ -4,14 +4,16 @@
     function bookSearchController($location, $http) {
         /* jshint validthis:true */
         var vm = this;
-        vm.searchBook = function() {
-            $http.get(vm.googleApiUrl + vm.searchQuery).then(function(result) {
-                vm.books= result.data.items;
+        vm.searchBook = function () {
+            vm.searchQuery = vm.searchQuery.replace('-', '').replace('_', '');
+            $http.get(vm.googleApiUrl + vm.searchQuery).then(function (result) {
+                vm.books = result.data.items;
+                vm.totalItems = result.data.totalItems;
             });
         }
         function activate() {
             vm.searchQuery = "";
-            vm.googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+            vm.googleApiUrl = "https://www.googleapis.com/books/v1/volumes?orderBy=relevance&&printType=books&&langRestrict=en&&maxResults=18&&q=";
         }
 
         activate();
